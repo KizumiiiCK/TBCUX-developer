30adfc56e571dcf5bcaf480daa4c8b61a5477c3e
Kizumiii <125788811+KizumiiiCK@users.noreply.github.com>
2026-05-01 00:02:13 +0800

    v1.1.12 Dev - pool date update.

--- Script/System/PoolSettings.cs
+++ Script/System/PoolSettings.cs
@@ -42,7 +42,7 @@ public class Pool
 
 public static class PoolSystemTime
 {
-    public static readonly DateTime openTime = new DateTime(year: 2026, month: 4, day: 30);
+    public static readonly DateTime openTime = new DateTime(year: 2026, month: 5, day: 1);
     public static DateTime Now => DateTime.Now;
 
     public static int DaysBetween(DateTime d1, DateTime d2) =>
